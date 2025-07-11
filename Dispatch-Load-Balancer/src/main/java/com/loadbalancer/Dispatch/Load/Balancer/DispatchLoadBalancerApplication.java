0213387a937447d14b6ba7e99324a70311567386
kalaskarp <kalaskarpratik22@gmail.com>
2025-07-11 18:20:44 +0530

    First Commit

---
+++ Dispatch-Load-Balancer/src/main/java/com/loadbalancer/Dispatch/Load/Balancer/DispatchLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.loadbalancer.Dispatch.Load.Balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchLoadBalancerApplication.class, args);
+	}
+
+}
---
+++ Dispatch-Load-Balancer/src/test/java/com/loadbalancer/Dispatch/Load/Balancer/DispatchLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadbalancer.Dispatch.Load.Balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
